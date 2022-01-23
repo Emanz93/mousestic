7035e78786ac0b69ad3f759c168fc030c9b35889
Emanz93 <emanuele.lovera@gmail.com>
2022-01-23 23:53:47 +0100

    Increased waiting time to 5 seconds

--- mousestic.py
+++ mousestic.py
@@ -11,14 +11,14 @@ MAX_X, MAX_Y = pyautogui.size()
 print('-----------------------------')
 print('|   Welcome to Mousestic!   |')
 print('-----------------------------')
-print('Press Ctrl-C to quit...')
+print('Press Ctrl-C to quit...\n')
 try:
     while True:
         r_x = random.randint(0, MAX_X)
         r_y = random.randint(0, MAX_Y)
 
         pyautogui.moveTo(x=r_x, y=r_y, duration=2)
-        time.sleep(3)
+        time.sleep(5)
 
 except (KeyboardInterrupt, pyautogui.FailSafeException) as e:
     ellapsed_time = time.gmtime(time.time() - start_time)
