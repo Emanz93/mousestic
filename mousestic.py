b89a689da020914d685948dacf26bce77dd7415f
Emanuele Lovera <Emanuele.Lovera@esa.int>
2022-02-06 14:52:02 +0100

    removed volume control in favour of capslock

--- mousestic.py
+++ mousestic.py
@@ -7,6 +7,8 @@ start_time = time.time()
 
 random.seed()
 MAX_X, MAX_Y = pyautogui.size()
+MAX_X = MAX_X - 100
+MAX_Y = MAX_Y - 100
 
 SLEEP_TIME = 2 # seconds
 CURSOR_SPEED = 2 # second
@@ -14,16 +16,15 @@ CURSOR_SPEED = 2 # second
 print('-----------------------------')
 print('|   Welcome to Mousestic!   |')
 print('-----------------------------')
-print('Cursor speed: {} seconds'.format(CURSOR_SPEED))
-print('Sleep time  : {} seconds'.format(SLEEP_TIME))
+#print('Cursor speed: {} seconds'.format(CURSOR_SPEED))
+#print('Sleep time  : {} seconds'.format(SLEEP_TIME))
 print('Press Ctrl-C to quit...\n')
 try:
     while True:
-        r_x = random.randint(0, MAX_X)
-        r_y = random.randint(0, MAX_Y)
-
-        pyautogui.moveTo(x=r_x, y=r_y, duration=2)
-        time.sleep(SLEEP_TIME)
+        pyautogui.press('capslock')
+        time.sleep(10)
+        pyautogui.press('capslock')
+        time.sleep(10)
 
 except (KeyboardInterrupt, pyautogui.FailSafeException) as e:
     ellapsed_time = time.gmtime(time.time() - start_time)
