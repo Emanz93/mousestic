2e61ab77c8bc16410dd988f66466334536d2c633
Emanuele Lovera <Emanuele.Lovera@ext.esa.int>
2023-01-09 11:32:51 +0100

    added ellapsed time and streamlined the entire script

--- mousestic.py
+++ mousestic.py
@@ -1,32 +1,19 @@
 import time
-from tracemalloc import start
 import pyautogui
-import random
 
 start_time = time.time()
-
-random.seed()
-MAX_X, MAX_Y = pyautogui.size()
-MAX_X = MAX_X - 100
-MAX_Y = MAX_Y - 100
-
-SLEEP_TIME = 2 # seconds
-CURSOR_SPEED = 2 # second
 
 print('-----------------------------')
 print('|   Welcome to Mousestic!   |')
 print('-----------------------------')
-#print('Cursor speed: {} seconds'.format(CURSOR_SPEED))
-#print('Sleep time  : {} seconds'.format(SLEEP_TIME))
-print('Press Ctrl-C to quit...\n')
 try:
     while True:
-        pyautogui.press('capslock')
-        time.sleep(10)
-        pyautogui.press('capslock')
-        time.sleep(10)
-
+        ellapsed_time = time.time() - start_time
+        print('\rPress Ctrl-C to quit... {}'.format(time.strftime("%H:%M:%S", time.gmtime(ellapsed_time))), end='', flush=True)
+        if int(ellapsed_time) % 10 == 0:
+            pyautogui.press('capslock')
+        time.sleep(1)
 except (KeyboardInterrupt, pyautogui.FailSafeException) as e:
-    ellapsed_time = time.gmtime(time.time() - start_time)
-    print('Total ellapsed time: {}'.format(time.strftime("%H:%M:%S", ellapsed_time)))
+    total_ellapsed_time = time.gmtime(time.time() - start_time)
+    print('\nTotal ellapsed time: {}'.format(time.strftime("%H:%M:%S", total_ellapsed_time)))
     print('Goodbye!')
